b0c1ac80cef73aeb00842da212ced4100f177206
lukas.pastva <glpat-jks7gpuTdLFqfyfH8Eb8>
2023-07-15 19:35:14 +0200

    Initial commit

--- source-code/bin/metrics.sh
+++ source-code/bin/metrics.sh
@@ -15,8 +15,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "1 day ago" -u +%s)
   end_time=$(date -u +%s)
   step="1d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_last_day{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_last_day{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last week
@@ -24,8 +24,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "last week monday" +%s)
   end_time=$(date -d "last week sunday 23:59:59" +%s)
   step="7d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_last_week{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_last_week{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last 7 days
@@ -33,8 +33,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "7 day ago" -u +%s)
   end_time=$(date -u +%s)
   step="7d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_last_7_days{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_last_7_days{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last 30 days
@@ -42,8 +42,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "30 day ago" -u +%s)
   end_time=$(date -u +%s)
   step="30d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_last_30_days{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_last_30_days{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime previous month
@@ -51,8 +51,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "$(date +%Y-%m-01) -1 month" +%s)
   end_time=$(date -d "$(date +%Y-%m-01) -1 second" +%s)
   step="30d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_previous_month{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_previous_month{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime three months ago
@@ -60,8 +60,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "3 months ago" +%s)
   end_time=$(date -u +%s)
   step="90d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_three_months_ago{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_three_months_ago{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime this year
@@ -69,8 +69,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "$(date +%Y)-01-01 00:00:00" +%s)
   end_time=$(date -u +%s)
   step="$(date +%j)d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_this_year{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_this_year{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime one year ago
@@ -78,8 +78,8 @@ for JOB in $JOBS; do
   start_time=$(date -d "1 year ago" +%s)
   end_time=$(date -u +%s)
   step="365d"
-  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  METRIC="uptime_exporter_one_year_ago{job=\"${JOB}\"} ${UPTIME}"
+  UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{prometheus_job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
+  METRIC="uptime_exporter_one_year_ago{prometheus_job=\"${JOB}\"} ${UPTIME}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 
 done
