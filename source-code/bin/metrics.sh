62610a3fee49b635bb28264c83414548fae980c5
lukas.pastva <glpat-jks7gpuTdLFqfyfH8Eb8>
2023-07-15 18:19:49 +0200

    Initial commit

--- source-code/bin/metrics.sh
+++ source-code/bin/metrics.sh
@@ -3,6 +3,10 @@ export $(xargs -0 -a "/proc/1/environ") 2>/dev/null
 
 source functions.inc.sh
 
+export EPOCH=$(date +%s)
+METRIC="uptime_exporter_heart_beat ${EPOCH}"
+METRICS=$(echo -e "$METRICS\n$METRIC")
+
 JOBS=$(curl -s "$PROMETHEUS_URL/api/v1/label/job/values" | jq -r '.data[]')
 for JOB in $JOBS; do
 
@@ -12,7 +16,8 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="1d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_last_day{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_last_day{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last week
   query="up{job=\"$JOB\"}"
@@ -20,7 +25,8 @@ for JOB in $JOBS; do
   end_time=$(date -d "last week sunday 23:59:59" +%s)
   step="7d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_last_week{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_last_week{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last 7 days
   query="up{job=\"$JOB\"}"
@@ -28,7 +34,8 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="7d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_last_7_days{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_last_7_days{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime last 30 days
   query="up{job=\"$JOB\"}"
@@ -36,7 +43,8 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="30d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_last_30_days{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_last_30_days{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime previous month
   query="up{job=\"$JOB\"}"
@@ -44,7 +52,8 @@ for JOB in $JOBS; do
   end_time=$(date -d "$(date +%Y-%m-01) -1 second" +%s)
   step="30d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_previous_month{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_previous_month{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime three months ago
   query="up{job=\"$JOB\"}"
@@ -52,7 +61,8 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="90d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_three_months_ago{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_three_months_ago{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime this year
   query="up{job=\"$JOB\"}"
@@ -60,7 +70,8 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="$(date +%j)d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_this_year{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_this_year{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
   # uptime one year ago
   query="up{job=\"$JOB\"}"
@@ -68,6 +79,10 @@ for JOB in $JOBS; do
   end_time=$(date -u +%s)
   step="365d"
   UPTIME=$(curl -s -G --data-urlencode "query=(avg(avg_over_time(up{job=\"${JOB}\"}[$step]) or on() vector(0) )*100)" --data-urlencode "start=$start_time" --data-urlencode "end=$end_time" --data-urlencode "step=$step" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[]?.value[1]')
-  echo "uptime_exporter_one_year_ago{job=\"${JOB}\"} ${UPTIME}"
+  METRIC="uptime_exporter_one_year_ago{job=\"${JOB}\"} ${UPTIME}"
+  METRICS=$(echo -e "$METRICS\n$METRIC")
 
 done
+
+rm /tmp/metrics.log || true
+echo -e $METRICS > /tmp/metrics.log
--- source-code/main.go
+++ source-code/main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
-	"os/exec"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	cmd, err := exec.Command("/bin/sh", "/usr/local/bin/metrics.sh").Output()
-	if err != nil {
-		fmt.Printf("error %s", err)
+	filePath := "/tmp/metrics.log"
+
+	// Check if file exists
+	if _, err := os.Stat(filePath); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist.")
+			return
+		}
 	}
-	output := string(cmd)
-	fmt.Fprintf(w, output)
+
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Echo contents of the file
+	_, err = io.Copy(os.Stdout, file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	//cmd, err := exec.Command("/bin/sh", "/usr/local/bin/metrics.sh").Output()
+	//if err != nil {
+	//	fmt.Printf("error %s", err)
+	//}
+	//output := string(cmd)
+	//fmt.Fprintf(w, output)
 }
 
 func main() {
