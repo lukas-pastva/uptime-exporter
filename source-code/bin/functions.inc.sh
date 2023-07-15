3b82856dd57e095bfa689b9089d47e404a1b232c
lukas.pastva <glpat-jks7gpuTdLFqfyfH8Eb8>
2023-07-15 15:36:13 +0200

    Initial commit

--- source-code/bin/functions.inc.sh
+++ source-code/bin/functions.inc.sh
@@ -5,20 +5,6 @@ function echo_message {
   echo -e "\n# $1"
 }
 
-function download_file {
-  echo_message "Downloading ${1} into ${2}"
-
-  if [ "${BUCKET_TYPE}" == "S3" ]; then
-      download_file_s3 "$1" "$2"
-  fi
-  if [ "${BUCKET_TYPE}" == "GCP" ]; then
-      download_file_gcp "$1" "$2"
-  fi
-}
-
 export EPOCH=$(date +%s)
 export DATE=$(date +"%Y-%m-%dT%H-%M-%SZ")
 export ANTI_DATE=$(( 10000000000 - $(date +%s) ))
-export BUCKET_TYPE=$(yq e '.uptime-exporter-config.bucket_type' /home/uptime-exporter-config.yaml)
-export BUCKET_NAME=$(yq e '.uptime-exporter-config.bucket_name' /home/uptime-exporter-config.yaml)
-export CONTAINER_ORCHESTRATOR=$(yq e '.uptime-exporter-config.container_orchestrator' /home/uptime-exporter-config.yaml)
--- source-code/bin/metrics.sh
+++ source-code/bin/metrics.sh
@@ -3,4 +3,8 @@ export $(xargs -0 -a "/proc/1/environ") 2>/dev/null
 
 source functions.inc.sh
 
-metrics-hw.sh
+query='up{job="your_job_name"}[30d]'
+response=$(curl -sG --data-urlencode "query=$query" "${PROMETHEUS_URL}")
+uptime_values=$(echo "$response" | grep -oE '\[[0-9]+.[0-9]+\]')
+
+echo "$uptime_values"
